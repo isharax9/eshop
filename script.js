7c50536af826bf7b3fae6877e8103262a9cd9b55
Ishara Lakshitha <isharax9@gmail.com>
2023-06-04 17:14:23 +0530

    done everything to opening forgot Password Model

---
+++ script.js
@@ -0,0 +1,107 @@
+function changeView() {
+
+    var signUpBox = document.getElementById("signUpBox");
+    var signInBox = document.getElementById("signInBox");
+
+    signUpBox.classList.toggle("d-none");
+    signInBox.classList.toggle("d-none");
+
+}
+
+function signUp() {
+
+    var f = document.getElementById("f");
+    var l = document.getElementById("l");
+    var e = document.getElementById("e");
+    var p = document.getElementById("p");
+    var m = document.getElementById("m");
+    var g = document.getElementById("g");
+
+    var form = new FormData;
+    form.append("f", f.value);
+    form.append("l", l.value);
+    form.append("e", e.value);
+    form.append("p", p.value);
+    form.append("m", m.value);
+    form.append("g", g.value);
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            var text = request.responseText;
+            if (text == "success") {
+                document.getElementById("msg").innerHTML = text;
+                document.getElementById("msg").className = "bi bi-check2-circle fs-5";
+                document.getElementById("alertdiv").className = "alert alert-success";
+                document.getElementById("msgdiv").className = "d-block";
+            } else {
+                document.getElementById("msg").innerHTML = text;
+                document.getElementById("msgdiv").className = "d-block";
+            }
+        }
+    }
+
+    request.open("POST", "signUpProcess.php", true);
+    request.send(form);
+
+}
+
+function signIn() {
+
+    var email = document.getElementById("email2");
+    var password = document.getElementById("password2");
+    var rememberme = document.getElementById("rememberme");
+
+    var f = new FormData();
+    f.append("e", email.value);
+    f.append("p", password.value);
+    f.append("r", rememberme.checked);
+
+    var r = new XMLHttpRequest();
+
+    r.onreadystatechange = function () {
+        if (r.readyState == 4) {
+            var t = r.responseText;
+            if (t == "success") {
+                window.location = "home.php";
+            } else {
+                document.getElementById("msg2").innerHTML = t;
+            }
+
+        }
+    };
+
+    r.open("POST", "signInProcess.php", true);
+    r.send(f);
+
+}
+
+var bm;
+function forgotPassword() {
+
+    var email = document.getElementById("email2");
+
+    var r = new XMLHttpRequest();
+
+    r.onreadystatechange = function () {
+        if (r.readyState == 4) {
+            var t = r.responseText;
+            if (t == "Success") {
+                alert("Verification code has sent to your email. Please check your inbox");
+                var m = document.getElementById("forgotPasswordModal");
+                bm = new bootstrap.Modal(m);
+                bm.show();
+            }else{
+                alert(t);
+            }
+            
+        }
+    }
+
+    r.open("GET", "forgotPasswordProcess.php?e=" + email.value, true);
+    r.send();
+
+
+
+}
